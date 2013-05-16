d80e7ddace00cb903e612fa3b961cab7558d5246
Bryant Yik <byik014@gmail.com>
2013-05-15 23:06:15 -0400

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'spec_helper'
 
 describe User do
@@ -29,6 +18,12 @@ describe User do
     it { should respond_to(:authenticate) }
     it { should respond_to(:microposts) }
     it { should respond_to(:feed) }
+    it { should respond_to(:relationships) }
+    it { should respond_to(:followed_users) }
+    it { should respond_to(:following?) }
+    it { should respond_to(:follow!) }
+    it { should respond_to(:reverse_relationships) }
+    it { should respond_to(:followers) }
 
     it { should be_valid }
     it { should_not be_admin }
@@ -42,90 +37,90 @@ describe User do
       it { should be_admin }
     end
 
-    describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
-    end
-
     describe "when name is not present" do
       before { @user.name = " " }
       it { should_not be_valid }
     end
 
     describe "when email is not present" do
-  	 before { @user.email = " " }
-  	 it { should_not be_valid }
+     before { @user.email = " " }
+     it { should_not be_valid }
     end
 
     describe "when name is too long" do
-  	 before { @user.name = "a" * 51 }
-  	 it {should_not be_valid }
+     before { @user.name = "a" * 51 }
+     it {should_not be_valid }
     end
 
     describe "when email format is invalid" do
-  	 it "should be valid" do
-  		  addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
-  		  addresses.each do |invalid_address|
-  			 @user.email = invalid_address
-  			 @user.should_not be_valid
-  		  end
-  	 end
+      it "should be valid" do
+        addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+        addresses.each do |invalid_address|
+         @user.email = invalid_address
+         @user.should_not be_valid
+        end
+      end
     end
 
     describe "when email format is valid" do
-  	 it "should be valid" do
-  		  addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-  		  addresses.each do |valid_address|
-  			 @user.email = valid_address
-  			 @user.should be_valid
-  		  end
-  	 end
+      it "should be valid" do
+        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+        addresses.each do |valid_address|
+          @user.email = valid_address
+          @user.should be_valid
+        end
+      end
     end
 
     describe "when email address is already taken" do
-  	 before do
-  		  user_with_same_email = @user.dup
-  		  user_with_same_email.email = @user.email.upcase
-  		  user_with_same_email.save
-  	 end
+     before do
+        user_with_same_email = @user.dup
+        user_with_same_email.email = @user.email.upcase
+        user_with_same_email.save
+     end
 
-  	 it { should_not be_valid }
+     it { should_not be_valid }
     end
 
     describe "when password is not present" do
-  	 before { @user.password = @user.password_confirmation = " " }
-  	 it { should_not be_valid }
+      before { @user.password = @user.password_confirmation = " " }
+      it { should_not be_valid }
     end
 
     describe "when password doesn't match confirmation" do
-  	 before { @user.password_confirmation = "mismatch" }
-  	 it {should_not be_valid }
+      before { @user.password_confirmation = "mismatch" }
+      it {should_not be_valid }
     end
 
     describe "when password confirmation is nil" do
-  	 before { @user.password_confirmation = nil }
-  	 it { should_not be_valid }
+      before { @user.password_confirmation = nil }
+      it { should_not be_valid }
     end
 
     describe "with a password that's too short" do
-  	 before { @user.password = @user.password_confirmation = "a * 5" }
-  	 it { should be_invalid }
+      before { @user.password = @user.password_confirmation = "a * 5" }
+      it { should be_invalid }
     end
 
     describe "return value of authenticate method" do
-  	 before { @user.save }
-  	 let(:found_user) { User.find_by_email(@user.email) }
+      before { @user.save }
+      let(:found_user) { User.find_by_email(@user.email) }
 
-  	 describe "with valid password" do
-  		  it { should == found_user.authenticate(@user.password) }
-  	 end
+      describe "with valid password" do
+        it { should == found_user.authenticate(@user.password) }
+     end
 
-  	 describe "with invalid password" do
-  		  let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+      describe "with invalid password" do
+        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-  		  it { should_not == user_for_invalid_password }
-  		  specify { user_for_invalid_password.should be_false }
-  	 end
+        it { should_not == user_for_invalid_password }
+        specify { user_for_invalid_password.should be_false }
+      end
+    end
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
     end
 
     describe "micropost associations" do
@@ -155,10 +150,44 @@ describe User do
         let(:unfollowed_post) do
           FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
         end
+        let(:followed_user) { FactoryGirl.create(:user) }
+
+        before do
+          @user.follow!(followed_user)
+          3.times { followed_user.microposts.create!(content: "Lorem ipsun") }
+        end
 
         its(:feed) { should include(newer_micropost) }
         its(:feed) { should include(older_micropost) }
         its(:feed) { should_not include(unfollowed_post) }
+        its(:feed) do
+          followed_user.microposts.each do |micropost|
+            should include(micropost)
+          end
+        end
       end
     end
-  end
+
+  describe "following" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        @user.save
+        @user.follow!(other_user)
+      end
+
+      it { should be_following(other_user) }
+      its(:followed_users) { should include(other_user) }
+
+      describe "followed user" do
+        subject { other_user }
+        its(:followers) { should include(@user) }
+      end
+      
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
+
+        it { should_not be_following(other_user) }
+        its(:followed_users) { should_not include(other_user) }
+      end
+    end
+end
